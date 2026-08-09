1ca5905000414a9fd5917af02d229db81a8096b0
bakarihamso429 <bakarihamso429@gmail.com>
2026-08-09 22:45:20 +0000

    Complete Story system with analytics, reactions and replies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import DomainSearch from "../components/DomainSearch";
-
+import HostingPlans from "../components/HostingPlans";
 function Home() {
   return (
       <>
             <Navbar />
                   <Hero />
                         <DomainSearch />
+                        <HostingPlans />
                             </>
                               );
                               }
